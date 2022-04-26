bd0605cef81c01a9f497a6c40f7097e4018b84a8
JBEDU16 <JBEDU16@DESKTOP-DM6IFR0>
2022-04-26 12:16:17 +0900

    커멘드

---
+++ src/main/java/com/yjj/board/command/BCommand.java
@@ -0,0 +1,10 @@
+package com.yjj.board.command;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public interface BCommand {
+	
+	void execute(HttpServletRequest request, HttpServletResponse response);
+	
+}
