588454af75daf9c5ff832c4ea0caa8da612f432c
JBEDU16 <JBEDU16@DESKTOP-DM6IFR0>
2022-04-26 12:15:59 +0900

    테스트

--- src/main/java/com/yjj/board/controller/BFrontController.java
+++ src/main/java/com/yjj/board/controller/BFrontController.java
@@ -1,6 +1,8 @@
 package com.yjj.board.controller;
 
 import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -40,6 +42,25 @@ public class BFrontController extends HttpServlet {
 	
 	private void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		
+		String viewPage = null;
+		
+		String uri = request.getRequestURI(); // 클라이언트가 요청한 url 전부 가져오기
+		String conPath = request.getContextPath(); //context 경로만 가져오기
+		String com = uri.substring(conPath.length()); // 전체 url 에서 context 경로 길이만큼 빼기
+		
+		if(com.equals("/test.do")) {
+			
+			request.setAttribute("id", "tiger");
+			
+			viewPage = "test.jsp";
+		} else if(com.equals("/delete.do")) {
+			
+		}
+		
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+		
 		
 	}
 	
